72a02429bd7fe08d8f07a05950bb05f4b5312855
August-Brandt <augustbrandt170@gmail.com>
2026-02-26 16:36:18 +0100

    feat: Improve run script with better output and add both algorithms

--- run.sh
+++ run.sh
@@ -1,29 +1,61 @@
 #! /bin/bash
 
-TEST_RUNS=10
+# Over all constants 
+TEST_RUNS=4
+ALGORITHMS=(1 2)
+ALGORITHMS_NAME=("concurrent" "countThenMove")
 RESULTS_FOLDER="experiment_resuts"
-CSV_HEADER="threads,hash bits,ms"
+
+CSV_HEADER="threads,hash bits,mean[ms]"
+
 
 mkdir "$RESULTS_FOLDER"
-mkdir "$RESULTS_FOLDER/concurrent"
-mkdir "$RESULTS_FOLDER/countThenMove"
-for thread in 1 2 4 8 16 32
+for algo in 0 1
 do
-    FILE_NAME="$RESULTS_FOLDER/concurrent/concurrent_${thread}t_timings.csv"
-    touch "$FILE_NAME"
-    echo "$CSV_HEADER" > "$FILE_NAME"
-    for hash_bit in {1..18}
+    mkdir "$RESULTS_FOLDER/${ALGORITHMS_NAME[$algo]}"
+    for thread in 1 2 4 8 16 32
     do
-        sum=0
-        echo "Running tests for $thread threads and $hash_bit hash bits"
-        for test_run in {0..9}
+        # Create csv file for a given number of threads
+        FILE_NAME="$RESULTS_FOLDER/${ALGORITHMS_NAME[$algo]}/${ALGORITHMS_NAME[$algo]}_${thread}t_timings.csv"
+        touch "$FILE_NAME"
+
+        # Set the header
+        echo -n "$CSV_HEADER" > "$FILE_NAME"
+        for i in $(seq 1 $TEST_RUNS)
         do
-            result=$(./out/main $thread $hash_bit 1 0 | grep -oE '[0-9]+')
-            sum=$(($sum + $result))
-            echo -e "\tTest $((test_run + 1)): $result ms"
+            echo -n ",run$i[ms]" >> "$FILE_NAME"
         done
-        mean=$(($sum / 10))
-        echo -e "\tMean: $mean"
-        echo "$thread,$hash_bit,$mean" >> "$FILE_NAME"
+        echo "" >> "$FILE_NAME"
+
+
+        for hash_bit in {1..18}
+        do
+            # Run TEST_RUNS number of run for pr params and save results 
+            runs={1..$TEST_RUNS}
+            echo "==== Running tests for ${ALGORITHMS_NAME[$algo]} at $thread threads and $hash_bit hash bits ===="
+            for test_run in $(seq 1 $TEST_RUNS)
+            do
+                result=$(./out/main $thread $hash_bit $ALGORITHMS[algo] 0 | grep -oE '[0-9]+')
+                runs[$(($test_run - 1))]=$result
+                echo -e "\tTest $test_run: $result ms"
+            done
+
+            # Calculate mean of runs
+            sum=0
+            for run_time in ${runs[@]}
+            do
+                sum=$(($sum + $run_time))
+            done
+            mean=$(($sum / $TEST_RUNS))
+
+            # Write results to file
+            echo -e "\tMean: $mean"
+            echo -n "$thread,$hash_bit,$mean" >> "$FILE_NAME"
+            for run_time in ${runs[@]}
+            do
+                echo -n ",$run_time" >> "$FILE_NAME"
+            done
+            echo "" >> "$FILE_NAME"
+        done
     done
 done
