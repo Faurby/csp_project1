9a38fa8db712b5d2ea6bd37c363a2be6be78b897
casperh123 <casperholten@protonmail.com>
2026-05-10 12:02:29 +0200

    Fix CPU script to use right locale

--- rodinia-run-cpu.sh
+++ rodinia-run-cpu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export LC_ALL=C
+export LANG=C
+
 RODINIA_PATH="$HOME/csprf/gpu-rodinia"
 BFS_PATH="$RODINIA_PATH/opencl/bfs"
 OUTPUT_PATH="$HOME/csprf"
@@ -13,12 +16,14 @@ echo ""
 echo "############# Starting CPU Experiment #############"
 
 # Write header once
-echo "graph_size,avg_exec_ms" > "$OUTPUT_FILE"
+echo "graph_size,avg_htod_ms,avg_exec_ms,avg_dtoh_ms" > "$OUTPUT_FILE"
 
-for datafile in "$DATA_PATH"/*
+for datafile in $(ls "$DATA_PATH"/* | sort -V)
 do
         BASENAME=$(basename "$datafile")
         EXEC_TMP=$(mktemp)
+        HTOD_TMP=$(mktemp)
+        DTOH_TMP=$(mktemp)
 
         # Extract graph size from filename e.g. graph8M.txt -> 8M
         GRAPH_SIZE=$(echo "$BASENAME" | sed 's/graph\(.*\)\.txt/\1/')
@@ -26,25 +31,29 @@ do
         # Warmup run (output discarded)
         echo "-------------------------"
         echo "Warmup run for $BASENAME (size: $GRAPH_SIZE) ..."
-        ./bfs.out -p 1 "$datafile" > /dev/null
+        ./bfs.out "$datafile" > /dev/null
 
         for ((test_run=1; test_run<=RUNS; test_run++))
         do
                 echo "Run $test_run/$RUNS on $BASENAME ..."
 
-                BFS_OUTPUT=$(./bfs.out -p 1 "$datafile")
+                BFS_OUTPUT=$(./bfs.out "$datafile")
+                echo "$BFS_OUTPUT" | awk '/^HtoD:/ { print $2 }' >> "$HTOD_TMP"
                 echo "$BFS_OUTPUT" | awk '/^Exec:/  { print $2 }' >> "$EXEC_TMP"
+                echo "$BFS_OUTPUT" | awk '/^DtoH:/ { print $2 }' >> "$DTOH_TMP"
         done
 
         echo "Finished $RUNS runs for $BASENAME"
 
         avg_col() { awk '{ sum += $1; count++ } END { if (count > 0) printf "%.4f", sum/count }' "$1"; }
 
+        AVG_HTOD=$(avg_col "$HTOD_TMP")
         AVG_EXEC=$(avg_col "$EXEC_TMP")
+        AVG_DTOH=$(avg_col "$DTOH_TMP")
 
-        echo "${GRAPH_SIZE},${AVG_EXEC}" >> "$OUTPUT_FILE"
+        echo "${GRAPH_SIZE},${AVG_HTOD},${AVG_EXEC},${AVG_DTOH}" >> "$OUTPUT_FILE"
 
-        rm "$EXEC_TMP"
+        rm "$HTOD_TMP" "$EXEC_TMP" "$DTOH_TMP"
 done
 
 echo "-------------------------"
